be2e8e60503c891e6a318e08ad0544f28d4fb534
happysalada <raphael@megzari.com>
2021-07-08 15:52:55 +0900

    sqlx: switch from query macro to regular functions for deployment

--- src/schema.rs
+++ src/schema.rs
@@ -13,65 +13,24 @@ fn unique_name(name: String) -> String {
     name.to_lowercase().replace(" ", "_")
 }
 
-#[derive(Clone, juniper::GraphQLObject)]
+#[derive(Clone, juniper::GraphQLObject, sqlx::FromRow)]
 #[graphql(description = "A plan")]
 struct Plan {
     id: String,
     title: String,
     description: Option<String>,
     agent_id: String,
-    inserted_at: DateTime<Utc>,
+    inserted_at: String,
 }
 
-#[derive(Clone, juniper::GraphQLObject)]
+#[derive(Clone, juniper::GraphQLObject, sqlx::FromRow)]
 #[graphql(description = "An agent")]
 struct Agent {
     id: String,
     name: String,
     unique_name: String,
     email: Option<String>,
-    inserted_at: DateTime<Utc>,
-}
-
-#[derive(Clone)]
-struct RawPlan {
-    id: String,
-    title: String,
-    description: Option<String>,
-    agent_id: String,
-    inserted_at: i64,
-}
-
-impl RawPlan {
-    fn to_graphql(self) -> Plan {
-        Plan {
-            id: self.id,
-            title: self.title,
-            description: self.description,
-            agent_id: self.agent_id,
-            inserted_at: timestamp_to_datetime(self.inserted_at),
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-struct RawAgent {
-    id: String,
-    name: String,
-    unique_name: String,
-    email: Option<String>,
-    inserted_at: i64,
-}
-impl RawAgent {
-    fn to_graphql(self) -> Agent {
-        Agent {
-            id: self.id,
-            name: self.name,
-            unique_name: self.unique_name,
-            email: self.email,
-            inserted_at: timestamp_to_datetime(self.inserted_at),
-        }
-    }
+    inserted_at: String,
 }
 
 pub struct QueryRoot;
@@ -80,30 +39,21 @@ pub struct QueryRoot;
 impl QueryRoot {
     #[graphql(description = "Get all Plans")]
     async fn plans(context: &Context, agent_id: String) -> FieldResult<Vec<Plan>> {
-        let plans = sqlx::query_as!(
-            RawPlan,
+        let plans = sqlx::query_as::<_, Plan>(
             "SELECT * FROM plans WHERE plans.agent_id = ? ORDER BY inserted_at DESC",
-            agent_id
         )
+        .bind(agent_id)
         .fetch_all(&context.pool)
         .await?;
-        Ok(plans
-            .iter()
-            .cloned()
-            .map(|raw_plan| raw_plan.to_graphql())
-            .collect())
+        Ok(plans.to_vec())
     }
 
     #[graphql(description = "Get all Agents")]
     async fn agents(context: &Context) -> FieldResult<Vec<Agent>> {
-        let agents = sqlx::query_as!(RawAgent, "SELECT * FROM agents ORDER BY inserted_at DESC")
+        let agents = sqlx::query_as::<_, Agent>("SELECT * FROM agents ORDER BY inserted_at DESC")
             .fetch_all(&context.pool)
             .await?;
-        Ok(agents
-            .iter()
-            .cloned()
-            .map(|raw_agent| raw_agent.to_graphql())
-            .collect())
+        Ok(agents.to_vec())
     }
 }
 
@@ -127,36 +77,34 @@ impl MutationRoot {
     #[graphql(description = "Add new plan")]
     async fn create_plan(context: &Context, new_plan: NewPlan) -> FieldResult<Plan> {
         let ulid = Ulid::new().to_string();
-        sqlx::query!(
-            "INSERT INTO plans (id, title, agent_id) VALUES (?, ?, ?)",
-            ulid,
-            new_plan.title,
-            new_plan.agent_id
-        )
-        .execute(&context.pool)
-        .await?;
-        let inserted_plan = sqlx::query_as!(RawPlan, "SELECT * FROM plans WHERE id = ?", ulid,)
+        sqlx::query("INSERT INTO plans (id, title, agent_id) VALUES (?, ?, ?)")
+            .bind(&ulid)
+            .bind(new_plan.title)
+            .bind(new_plan.agent_id)
+            .execute(&context.pool)
+            .await?;
+        let inserted_plan = sqlx::query_as::<_, Plan>("SELECT * FROM plans WHERE id = ?")
+            .bind(ulid)
             .fetch_one(&context.pool)
             .await?;
-        Ok(inserted_plan.to_graphql())
+        Ok(inserted_plan)
     }
 
     #[graphql(description = "Add new agent")]
     async fn create_agent(context: &Context, new_agent: NewAgent) -> FieldResult<Agent> {
         let ulid = Ulid::new().to_string();
         let unique_name: String = unique_name(new_agent.name.clone());
-        sqlx::query!(
-            "INSERT INTO agents (id, name, unique_name, email) VALUES (?, ?, ?, ?)",
-            ulid,
-            new_agent.name,
-            unique_name,
-            new_agent.email
-        )
-        .execute(&context.pool)
-        .await?;
-        let inserted_agent = sqlx::query_as!(RawAgent, "SELECT * FROM agents WHERE id = ?", ulid,)
+        sqlx::query("INSERT INTO agents (id, name, unique_name, email) VALUES (?, ?, ?, ?)")
+            .bind(&ulid)
+            .bind(new_agent.name)
+            .bind(unique_name)
+            .bind(new_agent.email)
+            .execute(&context.pool)
+            .await?;
+        let inserted_agent = sqlx::query_as::<_, Agent>("SELECT * FROM agents WHERE id = ?")
+            .bind(ulid)
             .fetch_one(&context.pool)
             .await?;
-        Ok(inserted_agent.to_graphql())
+        Ok(inserted_agent)
     }
 }
