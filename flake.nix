582322acf7a30813ca7d454f9a83255b94274a44
happysalada <raphael@megzari.com>
2021-07-09 16:40:12 +0900

    remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -4,46 +4,56 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        package_overlay = final: prev: {
-          vf-graphql-sqlite-backend = import ./package.nix { pkgs = final; inherit self; };
-        };
-        overlays = [ (import rust-overlay) package_overlay ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-      {
-        packages = {
-          inherit (pkgs) vf-graphql-sqlite-backend;
-        };
-        defaultPackage = pkgs.vf-graphql-sqlite-backend;
-        nixosModules.backend = import ./module.nix;
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            exa
-            fd
-            rust-bin.stable.latest.default
-            sqlite
-          ] ++ lib.optionals stdenv.isDarwin [
-            libiconv
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.CoreFoundation
-          ];
+  outputs = { self, nixpkgs, rust-overlay, ... }:
+    let
+      package_overlay = final: prev: {
+        vf-backend = import ./package.nix { pkgs = final; inherit self; };
+      };
+      # taken from https://github.com/ngi-nix/project-template/blob/master/flake.nix
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
 
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
-            export RUST_LOG=info
-            export DATABASE_URL=sqlite:db/try.db
-            export HTTP_PORT=8080
-          '';
-        };
-      }
-    );
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = self.overlays; });
+    in
+    {
+      overlays = [ (import rust-overlay) package_overlay ];
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) vf-backend;
+        });
+      defaultPackage = forAllSystems (system: self.packages.${system}.vf-backend);
+      nixosModules.backend = import ./module.nix;
+      devShell = forAllSystems
+        (system:
+          let
+            pkgs = nixpkgsFor.${system};
+          in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              exa
+              fd
+              rust-bin.stable.latest.default
+              sqlite
+            ] ++ lib.optionals stdenv.isDarwin [
+              libiconv
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.CoreFoundation
+            ];
+            shellHook = ''
+              alias ls=exa
+              alias find=fd
+              export RUST_LOG=info
+              export DATABASE_URL=sqlite:db/try.db
+              export HTTP_PORT=8080
+            '';
+          }
+        );
+
+    };
 }
